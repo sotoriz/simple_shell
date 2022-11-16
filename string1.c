19554060fc63ab22c55adcf8d53f7013ca19cc6c
Chukwuemeka Duruji <obioha.duruji@gmail.com>
2022-11-16 18:07:56 +0100

    Update string1.c

--- string1.c
+++ string1.c
@@ -1,4 +1,28 @@
+linee 1
+#include "shell.h"
 
+/**
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
+ */
+	
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
 /**
  * _strdup - duplicates a string
  * @str: the string to duplicate
@@ -22,24 +46,6 @@ char *_strdup(const char *str)
 	return (ret);
 }
 
-/**
- *_puts - prints an input string
- *@str: the string to be printed
- *
- * Return: Nothing
- */
-void _puts(char *str)
-{
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
 
 /**
  * _putchar - writes the character c to stdout
