037819720af40b2babd4f3e2b0e25321099c4042
Chukwuemeka Duruji <obioha.duruji@gmail.com>
2022-11-16 18:03:22 +0100

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -12,7 +12,25 @@ int hsh(info_t *info, char **av)
 	ssize_t r = 0;
 	int builtin_ret = 0;
 
-
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	
 	write_history(info);
 	free_info(info, 1);
 	if (!interactive(info) && info->status)
@@ -26,39 +44,6 @@ int hsh(info_t *info, char **av)
 	return (builtin_ret);
 }
 
-/**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
- *
- * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
- */
-int find_builtin(info_t *info)
-{
-	int i, built_in_ret = -1;
-	builtin_table builtintbl[] = {
-		{"exit", _myexit},
-		{"env", _myenv},
-		{"help", _myhelp},
-		{"history", _myhistory},
-		{"setenv", _mysetenv},
-		{"unsetenv", _myunsetenv},
-		{"cd", _mycd},
-		{"alias", _myalias},
-		{NULL, NULL}
-	};
-
-	for (i = 0; builtintbl[i].type; i++)
-		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
-		{
-			info->line_count++;
-			built_in_ret = builtintbl[i].func(info);
-			break;
-		}
-	return (built_in_ret);
-}
 
 /**
  * find_cmd - finds a command in PATH
