40e8fd0f8a1cc79d17e5b4fd26fd145bfa3f1e7f
Chukwuemeka Duruji <obioha.duruji@gmail.com>
2022-11-16 17:57:36 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -22,14 +22,29 @@ int is_cmd(info_t *info, char *path)
 	return (0);
 }
 
+
 /**
- * find_path - finds this cmd in the PATH string
- * @info: the info struct
+ * dup_chars - duplicates characters
  * @pathstr: the PATH string
- * @cmd: the cmd to find
+ * @start: starting index
+ * @stop: stopping index
  *
- * Return: full path of cmd if found or NULL
+ * Return: pointer to new buffer
  */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+
+
 char *find_path(info_t *info, char *pathstr, char *cmd)
 {
 	int i = 0, curr_pos = 0;
