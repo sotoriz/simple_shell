0cf05b3406f4490e671c5244e9a800f281a671b9
Chukwuemeka Duruji <obioha.duruji@gmail.com>
2022-11-16 18:13:25 +0100

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -16,7 +16,10 @@ char **strtow(char *str, char *d)
 		return (NULL);
 	if (!d)
 		d = " ";
-	
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+			numwords++;
+
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
@@ -30,13 +33,7 @@ char **strtow(char *str, char *d)
 		while (!is_delim(str[i + k], d) && str[i + k])
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
-		if (!s[j])
-		{
-			for (k = 0; k < j; k++)
-				free(s[k]);
-			free(s);
-			return (NULL);
-		}
+		
 		for (m = 0; m < k; m++)
 			s[j][m] = str[i++];
 		s[j][m] = 0;
