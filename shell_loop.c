4319209e325b6a2e6f164f6fa51bcb95342dcc22
Chukwuemeka Duruji <obioha.duruji@gmail.com>
2022-11-16 18:02:40 +0100

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -12,24 +12,7 @@ int hsh(info_t *info, char **av)
 	ssize_t r = 0;
 	int builtin_ret = 0;
 
-	while (r != -1 && builtin_ret != -2)
-	{
-		clear_info(info);
-		if (interactive(info))
-			_puts("$ ");
-		_eputchar(BUF_FLUSH);
-		r = get_input(info);
-		if (r != -1)
-		{
-			set_info(info, av);
-			builtin_ret = find_builtin(info);
-			if (builtin_ret == -1)
-				find_cmd(info);
-		}
-		else if (interactive(info))
-			_putchar('\n');
-		free_info(info, 0);
-	}
+
 	write_history(info);
 	free_info(info, 1);
 	if (!interactive(info) && info->status)
