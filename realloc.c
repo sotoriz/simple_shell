53c21b9b2faf30ab037eda17093d50de429c5a66
Chukwuemeka Duruji <obioha.duruji@gmail.com>
2022-11-16 17:59:02 +0100

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -16,20 +16,6 @@ char *_memset(char *s, char b, unsigned int n)
 	return (s);
 }
 
-/**
- * ffree - frees a string of strings
- * @pp: string of strings
- */
-void ffree(char **pp)
-{
-	char **a = pp;
-
-	if (!pp)
-		return;
-	while (*pp)
-		free(*pp++);
-	free(a);
-}
 
 /**
  * _realloc - reallocates a block of memory
